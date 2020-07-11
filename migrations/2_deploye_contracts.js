c1c8c3bbf68d295083b93c52a28dbf4f36133614
parv3213 <parv3213@gmail.com>
2020-07-11 08:02:08 +0530

    added Token.sol, transfered token to EthSwap

---
+++ migrations/2_deploye_contracts.js
@@ -0,0 +1,15 @@
+const Token = artifacts.require("Token");
+const EthSwap = artifacts.require("EthSwap");
+
+module.exports = async function(deployer) {
+	// Deploy Token
+	await deployer.deploy(Token);
+	const token = await Token.deployed();
+
+	// Deploy EthSwap
+	await deployer.deploy(EthSwap);
+	const ethSwap = await EthSwap.deployed();
+
+	// Transfer all the tokens to EthSwap (1 M)
+	await token.transfer(ethSwap.address, "1000000000000000000000000");
+};
