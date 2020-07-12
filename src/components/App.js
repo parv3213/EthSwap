02ebc495e2561f00b10edba42f76ba95658ad308
parv3213 <parv3213@gmail.com>
2020-07-12 08:44:02 +0530

    navbar component

--- src/components/App.js
+++ src/components/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
 import Web3 from "web3";
+import Navbar from "./Navbar";
 import "./App.css";
 
 class App extends Component {
@@ -45,16 +46,7 @@ class App extends Component {
 	render() {
 		return (
 			<div>
-				<nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-					<a
-						className="navbar-brand col-sm-3 col-md-2 mr-0"
-						href="http://www.dappuniversity.com/bootcamp"
-						target="_blank"
-						rel="noopener noreferrer"
-					>
-						Dapp University
-					</a>
-				</nav>
+				<Navbar account={this.state.account} />
 				<div className="container-fluid mt-5">
 					<div className="row">
 						<main role="main" className="col-lg-12 d-flex text-center">
