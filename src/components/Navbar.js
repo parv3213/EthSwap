98244d4315acfb9e12cf62ec07935e4f167dbfe7
parv3213 <parv3213@gmail.com>
2020-07-19 18:57:42 +0530

    rearrange

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,12 +5,7 @@ class Navbar extends Component {
 	render() {
 		return (
 			<nav className="navbar navbar-dark fixed-top bg-dark flex-md-nowrap p-0 shadow">
-				<a
-					className="navbar-brand col-sm-3 col-md-2 mr-0"
-					href="http://www.dappuniversity.com/bootcamp"
-					target="_blank"
-					rel="noopener noreferrer"
-				>
+				<a className="navbar-brand col-sm-3 col-md-2 mr-0" href="#" target="_blank" rel="noopener noreferrer">
 					EthSwap
 				</a>
 				<ul className="navbar-nav px-3">
